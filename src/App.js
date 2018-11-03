d23fa0087350f58c42a1fdac624138d37e286738
oneEyedBunny <allyson.short@gmail.com>
2018-11-02 22:03:17 -0700

    Switch fonts to formula calc

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ class App extends React.Component {
     console.log('isLoggedin =', this.props.user.isLoggedin);
     return (
       <div className='app'>
-        <Navigation zztop={this.isLoggedIn}/>
+        <div className="page-1-view">
+        <Navigation />
 
         <div id='welcome-message-container'>
           <h3 className='welcome-quote'> Hello. Welcome to Trade Tally</h3>
@@ -41,6 +42,7 @@ class App extends React.Component {
           <div className='chevron'></div>
           <div className='chevron'></div>
         </div>
+       </div>
 
         <section className='half-boxs-container wow'>
           <div className='left-box wow zoomIn'>
@@ -60,36 +62,36 @@ class App extends React.Component {
                 <a href='#display-login-form'>Enter a Trade</a>}
 
                 </div>
-                <div className='option-box'>
-                  {this.props.user.isLoggedin ?
-                    <Link className='option-box-text' to='/trade-summary'>See All Trades</Link>:
-                      <a href='#display-login-form'>See All Trades</a>}
-                      </div>
-                    </section>
-                    <section className='how-it-works wow fadeInLeft' id='how-it-works-summary'>
-                      <h3> How to use the app</h3>
-                      <img src='images/addblu.png' alt='create account' className='what-to-do-gif'
-                        onClick={() => this.newAccountForm()}/>
-                      <p className='create-account-link how-it-works-text' onClick={() => this.newAccountForm()}>
-                        Create an Account
-                      </p>
-                      {this.state.createAccountDisplay && <CreateAccount />}
-                      {/*checks if both are true, if they are, render them. CreateAccount will always true */}
-                      <img src='images/searchblu.png' alt='enter a trade' className='what-to-do-gif' />
-                      <p className='how-it-works-text'> Find the people you trade with & enter your trades when they happen</p>
-                      <img src='images/trkblu.png' alt='see your trades' className='what-to-do-gif' />
-                      <p className='how-it-works-text'> Then see your trade history with all your partners as well as the details of the individual trades so you know who owes who </p>
-                    </section>
+          <div className='option-box'>
+            {this.props.user.isLoggedin ?
+              <Link className='option-box-text' to='/trade-summary'>See All Trades</Link>:
+                <a href='#display-login-form'>See All Trades</a>}
+                </div>
+          </section>
+          <section className='how-it-works wow fadeInLeft' id='how-it-works-summary'>
+            <h3> How to use the app</h3>
+            <img src='images/addblu.png' alt='create account' className='what-to-do-gif'
+              onClick={() => this.newAccountForm()}/>
+            <p className='create-account-link how-it-works-text' onClick={() => this.newAccountForm()}>
+              Create an Account
+            </p>
+            {this.state.createAccountDisplay && <CreateAccount />}
+            {/*checks if both are true, if they are, render them. CreateAccount will always true */}
+            <img src='images/searchblu.png' alt='enter a trade' className='what-to-do-gif' />
+            <p className='how-it-works-text'> Find the people you trade with & enter your trades when they happen</p>
+            <img src='images/trkblu.png' alt='see your trades' className='what-to-do-gif' />
+            <p className='how-it-works-text'> Then see your trade history with all your partners as well as the details of the individual trades so you know who owes who </p>
+          </section>
 
 
-                    <footer role='contentinfo' id='footer'>
-                      <h5 className='footer-info'>Built by <a href='http://www.linkedin.com/in/allyson-short/' target='_blank'>
-                        Allyson Short</a></h5>
-                    </footer>
-                  </div>
-                );
-              }
-            };
+          <footer role='contentinfo' id='footer'>
+            <h5 className='footer-info'>Built by <a href='http://www.linkedin.com/in/allyson-short/' target='_blank'>
+              Allyson Short</a></h5>
+          </footer>
+      </div>
+      );
+    }
+  };
 
 const mapStateToProps = state => {
   return {
